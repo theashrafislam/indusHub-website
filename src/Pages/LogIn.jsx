ac52a35b0f33ae113ae42ffe69f713e5c69da131
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-04-13 13:04:09 +0600

    login page design is complate

--- src/Pages/LogIn.jsx
+++ src/Pages/LogIn.jsx
@@ -1,7 +1,37 @@
+import { useState } from "react";
+import { IoEye, IoEyeOff } from "react-icons/io5";
+import { Link } from "react-router-dom";
+
 const LogIn = () => {
+    const [showPass, setShowPass] = useState(false);
     return (
-        <div>
-            <h1>Hello, I am Login pages</h1>
+        <div className="mt-4">
+            <div className="text-center mx-4 my-3">
+                <h1 className="text-xl md:text-3xl font-bold">Login Now</h1>
+                <p className="text-lg">Welcome back to the realm of connection and exploration. Log in to unlock personalized experiences, connect with like-minded individuals, and embark on your digital journey with us.</p>
+            </div>
+            <div className="px-4 lg:max-w-7xl mx-auto">
+                <form className="space-y-4">
+                    <div className="flex flex-col">
+                        <label htmlFor="email" className="text-lg">Email</label>
+                        <input type="email" name="email" id="email" className="border-2 p-2" placeholder="Enter your email"/>
+                    </div>
+                    <div className="flex flex-col relative">
+                        <label htmlFor="password" className="text-lg">Password</label>
+                        <input type={showPass ? "text" : "password"} name="password" id="password" className=" border-2 p-2" placeholder="Enter your password" />
+                        <span onClick={() => setShowPass(!showPass)} className="absolute right-2 bottom-2">
+                            {
+                                showPass ? <IoEye className="text-2xl md:text-3xl"/> : <IoEyeOff className="text-2xl md:text-3xl"/>
+                            }
+                        </span>
+                    </div>
+                    <div className="flex flex-col">
+                        <button className="btn w-full font-bold text-lg bg-violet-600 text-gray-50 hover:text-black">Login</button>
+                    </div>
+                </form>
+                <p className="font-semibold mt-2">If you haven't an account. So, please <Link to="/register" className="text-red-500">Registration</Link></p>
+
+            </div>
         </div>
     );
 };
--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -3,26 +3,41 @@ import { Link } from "react-router-dom";
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
-const lengthWarning = () => toast.warning("Password must be at least 6 characters.");
-const uppercaseWarning = () => toast.warning("Must have an Uppercase letter in the password");
-const lowercaseWarning = () => toast.warning("Must have a Lowercase letter in the password");
+import { IoEye } from "react-icons/io5";
+import { IoEyeOff } from "react-icons/io5";
+import { useState } from "react";
+
+const lengthWarning = () => toast.warning("Your password must be at least six characters long.");
+const uppercaseWarning = () => toast.warning("Please include at least one uppercase letter in your password.");
+const lowercaseWarning = () => toast.warning("Your password must contain at least one lowercase letter.");
 
 const Register = () => {
+
+    const [showPass, setShowPass] = useState(false);
+
+
+
+    // const handleShow = () => {
+    //     setShowPass(false)
+    // }
+    // const handleHide = () => {
+    //     setShowPass(true)
+    // }
 
 
     const { register, handleSubmit, watch, formState: { errors } } = useForm();
     const onSubmit = data => {
-        const {fullName, email, photo, password} = data;
-        
-        if(password.length < 6){
+        const { fullName, email, photo, password } = data;
+
+        if (password.length < 6) {
             lengthWarning();
-            return; 
+            return;
         }
-        else if(!/(?=.*?[A-Z])/.test(password)){
+        else if (!/(?=.*?[A-Z])/.test(password)) {
             uppercaseWarning();
             return;
         }
-        else if(!/(?=.*?[a-z])/.test(password)){
+        else if (!/(?=.*?[a-z])/.test(password)) {
             lowercaseWarning();
             return;
         }
@@ -31,30 +46,36 @@ const Register = () => {
     return (
         <div className="mt-4">
             <div className="text-center mx-4 my-3">
-                <h1 className="text-xl md:text-3xl font-bold">Register Now</h1>
+                <h1 className="text-xl md:text-3xl font-bold">Registration Now</h1>
                 <p className="text-lg">Welcome to our registration page! Registering with us opens up a world of opportunities and benefits. Whether you're a new user or returning member, <br /> creating an account is quick and easy. Join our community today and unlock exclusive features!</p>
             </div>
             <div className="px-4 lg:max-w-7xl mx-auto">
                 <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                     <div className="flex flex-col">
                         <label htmlFor="name" className="text-lg">Full Name</label>
-                        <input type="text" name="name" id="name" className="border-2 p-2" placeholder="Full Name" {...register("fullName", { required: true })}/>
+                        <input type="text" name="name" id="name" className="border-2 p-2" placeholder="Full Name" {...register("fullName", { required: true })} />
                         {errors.fullName && <span className="mt-2 text-red-500">This field is required.</span>}
-                        
+
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="email" className="text-lg">Email</label>
-                        <input type="email" name="email" id="email" className="border-2 p-2" placeholder="Enter your email" {...register("email", { required: true })}/>
+                        <input type="email" name="email" id="email" className="border-2 p-2" placeholder="Enter your email" {...register("email", { required: true })} />
                         {errors.email && <span className="mt-2 text-red-500">This field is required.</span>}
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="photo" className="text-lg">Photo URL</label>
-                        <input type="text" name="photo" id="photo" className="border-2 p-2" placeholder="Enter your photo URL" {...register("photo", { required: true })}/>
+                        <input type="text" name="photo" id="photo" className="border-2 p-2" placeholder="Enter your photo URL" {...register("photo", { required: true })} />
                         {errors.photo && <span className="mt-2 text-red-500">This field is required.</span>}
                     </div>
-                    <div className="flex flex-col">
+                    <div className="flex flex-col relative">
                         <label htmlFor="password" className="text-lg">Password</label>
-                        <input type="password" name="password" id="password" className="border-2 p-2" placeholder="Enter your password" {...register("password", { required: true })}/>
+                        <input type={showPass ? "text" : "password"} name="password" id="password" className=" border-2 p-2" placeholder="Enter your password" {...register("password", { required: true })}
+                        />
+                        <span onClick={() => setShowPass(!showPass)} className="absolute right-2 bottom-2">
+                            {
+                                showPass ? <IoEye className="text-2xl md:text-3xl"/> : <IoEyeOff className="text-2xl md:text-3xl"/>
+                            }
+                        </span>
                         {errors.password && <span className="mt-2 text-red-500">This field is required.</span>}
                     </div>
                     <div className="flex flex-col">
@@ -62,7 +83,7 @@ const Register = () => {
                     </div>
                 </form>
                 <p className="font-semibold mt-2">If you already have an account. Please <Link to="/login" className="text-red-500">Log In</Link></p>
-                
+
             </div>
             <ToastContainer />
         </div>
