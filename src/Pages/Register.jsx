b0de86481bf0ae82b09a6b1e352d52bf38b0d986
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-04-13 10:48:31 +0600

    register page is responsive design is complate

--- src/Pages/Register.jsx
+++ src/Pages/Register.jsx
@@ -1,6 +1,11 @@
+import { useForm } from "react-hook-form";
 import { Link } from "react-router-dom";
 
 const Register = () => {
+    const { register, handleSubmit, watch, formState: { errors } } = useForm();
+    const onSubmit = data => {
+        console.log(data)
+    };
     return (
         <div className="mt-4">
             <div className="text-center mx-4">
@@ -8,22 +13,23 @@ const Register = () => {
                 <p className="text-lg">Welcome to our registration page! Registering with us opens up a world of opportunities and benefits. Whether you're a new user or returning member, <br /> creating an account is quick and easy. Join our community today and unlock exclusive features!</p>
             </div>
             <div className="px-4 lg:max-w-7xl mx-auto">
-                <form className="space-y-4">
+                <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
                     <div className="flex flex-col">
                         <label htmlFor="name" className="text-lg">Full Name</label>
-                        <input type="text" name="name" id="name" className="border-2 p-2" placeholder="Full Name"/>
+                        <input type="text" name="name" id="name" className="border-2 p-2" placeholder="Full Name" {...register("fullName", { required: true })}/>
+                        {errors.fullName && <span>This field is required</span>}
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="email" className="text-lg">Email</label>
-                        <input type="email" name="email" id="email" className="border-2 p-2" placeholder="Enter your email"/>
+                        <input type="email" name="email" id="email" className="border-2 p-2" placeholder="Enter your email" />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="photo" className="text-lg">Photo URL</label>
-                        <input type="text" name="photo" id="photo" className="border-2 p-2" placeholder="Enter your photo URL"/>
+                        <input type="text" name="photo" id="photo" className="border-2 p-2" placeholder="Enter your photo URL" />
                     </div>
                     <div className="flex flex-col">
                         <label htmlFor="password" className="text-lg">Password</label>
-                        <input type="password" name="password" id="password" className="border-2 p-2" placeholder="Enter your password"/>
+                        <input type="password" name="password" id="password" className="border-2 p-2" placeholder="Enter your password" />
                     </div>
                     <div className="flex flex-col">
                         <button className="btn w-full font-bold text-lg bg-violet-600 text-gray-50 hover:text-black">Registration</button>
