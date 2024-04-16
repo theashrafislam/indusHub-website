322e518983310327662a7af4ddecce93b01599c7
Ashraful Islam <155339396+theashrafislam@users.noreply.github.com>
2024-04-16 08:31:36 +0600

    some bug fixed and push fianl version

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -26,7 +26,7 @@ const router = createBrowserRouter([
             },
             {
                 path: "/userProfile",
-                element: <UserProfile></UserProfile>
+                element: <PrivateRoute><UserProfile></UserProfile></PrivateRoute>
             },
             {
                 path: "/contactUs",
